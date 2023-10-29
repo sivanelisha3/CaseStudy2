ff52571aca73d2a35b2e6dbcbbe8b30ccc7a9a4d
sivanelisha <sivanelisha@10.232.188.88>
2023-10-29 17:52:23 -0500

    part 2 - case data and sird model on the same plot

--- part2.m
+++ part2.m
@@ -1,23 +1,54 @@
+clear;
+close all;
+
+%%%%%%%%% Part 1 Data Fit %%%%%%%%%
+x_t = [0.75 0.10 0.10 0.05];
+
+A = [.95 .04 .3 0;
+    .05 .85 0 0;
+    0 .1 .7 0;
+    0 .01 0 1];
+
+% initial condition: 
+x0 = [1; 0; 0; 0]; 
+
+num_days = 100; 
+
+X = zeros(4, num_days);
+X(:,1) = x0;
+
+% allocate
+for t = 2:num_days
+    X(:,t) = A * X(:,t-1);
+end
+
 % Step 1: Load the data from the 'COVID_STL.mat' file
 load('COVID_STL.mat');
+
+% Look at first 100 days
+selected_dates = dates(1:100);
 
 % Step 2: Normalize cases and deaths by population
 normalized_cases = cases_STL / POP_STL;
 normalized_deaths = deaths_STL / POP_STL;
 
-% Step 3: Create a plot of normalized cases and deaths vs. dates on the same figure
+% Create a single plot
 figure;
-plot(dates, normalized_cases, 'b', 'LineWidth', 2);
-hold on;
-plot(dates, normalized_deaths, 'r', 'LineWidth', 2);
 
-% Customize the plot
-title('Normalized COVID Cases and Deaths in St. Louis');
+% Epidemic Dynamics Over Time
+plot(selected_dates, X(1,1:100), 'b', 'LineWidth', 2); hold on;
+plot(selected_dates, X(2,1:100), 'r', 'LineWidth', 2);
+plot(selected_dates, X(3,1:100), 'g', 'LineWidth', 2);
+plot(selected_dates, X(4,1:100), 'k', 'LineWidth', 2);
+
+% Normalized COVID Cases and Deaths in St. Louis for first 100 days
+plot(selected_dates, normalized_cases(1:100), '--r', 'LineWidth', 2); 
+plot(selected_dates, normalized_deaths(1:100), '--k', 'LineWidth', 2);
+
+% Plot
+title('Comparison: Epidemic Dynamics and COVID Cases/Deaths in St. Louis (First 100 days)');
 xlabel('Date');
-ylabel('Normalized Cases/Deaths');
-legend('Normalized Cases', 'Normalized Deaths');
+ylabel('Population Fraction/Normalized Value');
+legend('Susceptible', 'Infected', 'Recovered', 'Deceased', 'Normalized Cases', 'Normalized Deaths');
 grid on;
-
-
-% Display the figure
 hold off;
