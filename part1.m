2f2514d4443ef45de2c7afa43ca403dcd9b9166f
sivanelisha3 <126642457+sivanelisha3@users.noreply.github.com>
2023-11-03 14:56:17 -0500

    Update part1.m

--- part1.m
+++ part1.m
@@ -1,33 +1,45 @@
 clear;
 close all;
 
-x_t = [0.75 0.10 0.10 0.05];
-
+% System matrix
 A = [.95 .04 .3 0;
-    .05 .85 0 0;
-    0 .1 .7 0;
-    0 .01 0 1];
+     .05 .85 0 0;
+     0 .1 .7 0;
+     0 .01 0 1];
 
-% initial condition: 
+% No input -- B is a zero matrix
+B = zeros(4,1); 
+
+% C is an identity matrix
+C = eye(4); 
+
+% D is zero
+D = zeros(4,1);
+
+% Sample time is 1 day
+sample_time = 1;
+
+% Create state-space object
+sys = ss(A, B, C, D, sample_time);
+
+% Initial condition: 
 x0 = [1; 0; 0; 0]; 
 
+% Time vector
 num_days = 100; 
+t = 0:num_days-1; 
 
-X = zeros(4, num_days);
-X(:,1) = x0;
+u = zeros(num_days, 1);
 
-% Directly calculate using matrix power
-for t = 2:num_days
-    At = A^t;
-    X(:,t) = At * x0;
-end
+% Simulate the system
+[Y, T, X] = lsim(sys, u, t, x0);
 
-% plot
+% Plot
 figure;
-plot(1:num_days, X(1,:), 'b', 'LineWidth', 2); hold on;
-plot(1:num_days, X(2,:), 'r', 'LineWidth', 2);
-plot(1:num_days, X(3,:), 'g', 'LineWidth', 2);
-plot(1:num_days, X(4,:), 'k', 'LineWidth', 2);
+plot(T, X(:,1), 'b', 'LineWidth', 2); hold on;
+plot(T, X(:,2), 'r', 'LineWidth', 2);
+plot(T, X(:,3), 'g', 'LineWidth', 2);
+plot(T, X(:,4), 'k', 'LineWidth', 2);
 legend('Susceptible', 'Infected', 'Recovered', 'Deceased');
 xlabel('Days');
 ylabel('Population Fraction');
